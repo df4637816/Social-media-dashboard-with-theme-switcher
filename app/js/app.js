48de7163e79eb0c5bf3885a37b8998483c936385
張子良 <df4637816@zhangziliangdeMacBook-Pro.local>
2023-02-17 20:49:39 +0800

    修改＆新增修改＆新增card componets

--- app/js/app.js
+++ app/js/app.js
@@ -1,1 +1,9 @@
-console.log("hello")
+const toggle = document.querySelector('.toggle')
+const toggleButton = document.querySelector('.toggle__button')
+const  body = document.querySelector('body')
+
+toggle.addEventListener('click',function(){
+      toggle.classList.toggle('active');
+      body.classList.toggle('active');
+      
+      })
